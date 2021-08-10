4b99b15ce3b2ac7d6a149c4efef710220b7f65e8
Ismayilmammadov <ismammadov@gmail.com>
2021-08-10 12:29:28 +0400

    commit1

---
+++ homework-7/java.js
@@ -0,0 +1,22 @@
+let music = ["rock", "disco", "pop", "national"]
+let musictype = prompt ('what is your favorite music band?')
+
+
+
+// if (musictype === music[2])
+// alert ('YEAH I LOVE THEM!')
+
+// else
+
+// alert ("Nah. They're pretty lame.")
+
+
+// if (music.indexOf[musictype.toLowerCase()] === music.indexOf['disco'])
+
+if (musictype.toLowerCase() === music[2])
+
+alert ('YEAH I LOVE THEM!')
+
+else 
+
+alert ("Nah. They're pretty lame.")
